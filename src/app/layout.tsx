8273febd5eb829ac2fe342bda0823e703db6239a
roleary <ronanchristopher.oleary@teampicnic.com>
2024-02-15 22:42:02 +0100

    small updates

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "I truly hate Docker",
+  title: "NxtDkr",
   description: "What a pain in the hoop",
 };
 
