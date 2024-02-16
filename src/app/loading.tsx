ab239b1eb7eccc53cc8fa02417bcaf208ee7b023
roleary <ronanchristopher.oleary@teampicnic.com>
2024-02-16 11:40:36 +0100

    Setup(Initial Scaffold): Bells and whistles Should be base template

---
+++ src/app/loading.tsx
@@ -0,0 +1,6 @@
+export default function Loading() {
+    // You can add any UI inside Loading, including a Skeleton.
+    return (
+    <h1 className="text-3xl font-black justify-center content-center">WE LOAD!!!</h1>
+    )
+  }
