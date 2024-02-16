68b2ffca6c2c60a2c565aa5bbac6c6bafdeb1e7d
roleary <ronanchristopher.oleary@teampicnic.com>
2024-02-16 17:33:52 +0100

    fix(nxtdkt-fix-0005): small fix on the types in nav
    small fix on nav

--- src/components/ui/nav.tsx
+++ src/components/ui/nav.tsx
@@ -7,7 +7,7 @@ import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import React, { useState } from 'react';
 
-export const Nav = ({ menu }) => {
+export const Nav = ({ menu }: any) => {
   const isMobile = useState(false);
   const currentRoute = usePathname();
   const [navActive, setNavActive] = useState(false);
@@ -40,15 +40,6 @@ export const Nav = ({ menu }) => {
             <div />
           </div>
           <ul className={`nav__menu-list px-4 ${navActive ? 'active' : ''}`}>
-            {/* {isMobile ? 
-                    <div className={'flex'}>
-                        <input className={'mobileSearch'} type={'text'} name={'search'} placeholder={'Search...'} />
-                        <button className="btn inline-block px-6 py-2.5 bg-red-500 text-white font-medium text-xs leading-tight uppercase rounded-r rounded-l-none shadow-md hover:bg-red-400 hover:shadow-lg focus:bg-red-500  focus:shadow-lg focus:outline-none focus:ring-0 active:bg-red-500 active:shadow-lg transition duration-150 ease-in-out flex items-center" type="button" id="button-addon2">
-                            <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="search" className="w-4" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-                                <path fill="currentColor" d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z"></path>
-                            </svg>
-                        </button>
-                    </div> : ''} */}
             <Link
               href="/info"
               className={`nav__link text-white font-black ${
@@ -59,7 +50,7 @@ export const Nav = ({ menu }) => {
             >
               INFO
             </Link>
-            {menu.map((link, _id) => (
+            {menu.map((link: any, _id: number) => (
               // @ts-ignore
               // eslint-disable-next-line react/no-array-index-key
               <li id={_id} key={_id}>
