e4ed084cf709f655ca4a42fef7dbc78572a65c2f
Ronan <roleary81@gmail.com>
2024-02-28 23:16:43 +0100

    feat(nxtdkr-feat-0023): add hooks, checking and validation
    okay, so there are more than a few ts-ignores in there, but sherrup yew ye tick

---
+++ src/hooks/useDelay.ts
@@ -0,0 +1,18 @@
+import { useState, useEffect } from 'react'
+
+export const useDelay = (delayTime: unknown) => {
+	const [done, setDone] = useState(false)
+  
+	useEffect(() => {
+		const delay = setTimeout(() => {
+			setDone(true)
+	// @ts-expect-error
+    }, delayTime)
+
+		return () => clearTimeout(delay)
+	}, [delayTime])
+
+	return done
+}
+
+export default useDelay
