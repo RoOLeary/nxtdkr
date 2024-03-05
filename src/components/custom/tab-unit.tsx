38ba910930baa3aaf0dfd3953fb9289546b4f822
Ronan O'Leary <roleary81@gmail.com>
2024-03-05 15:21:44 +0100

    fix(nxtdkr-fix-00XX): update tabs dependencies

--- src/components/custom/tab-unit.tsx
+++ src/components/custom/tab-unit.tsx
@@ -1,6 +1,8 @@
-"use client"
+/* eslint-disable react/jsx-no-comment-textnodes */
 
-import { Button } from "@/components/ui/button"
+'use client';
+
+import { Button } from '@/components/ui/button';
 import {
   Card,
   CardContent,
@@ -8,16 +10,11 @@ import {
   CardFooter,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
-import {
-  Tabs,
-  TabsContent,
-  TabsList,
-  TabsTrigger,
-} from "@/components/ui/tabs"
- 
+} from '@/components/ui/card';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
+
 export function TabUnit() {
   return (
     <Tabs defaultValue="account" className="w-[400px]">
@@ -30,7 +27,8 @@ export function TabUnit() {
           <CardHeader>
             <CardTitle>Account</CardTitle>
             <CardDescription>
-              Make changes to your account here. Click save when you're done.
+              Make changes to your account here. Click save when you&apos;re
+              done.
             </CardDescription>
           </CardHeader>
           <CardContent className="space-y-2">
@@ -53,7 +51,8 @@ export function TabUnit() {
           <CardHeader>
             <CardTitle>Password</CardTitle>
             <CardDescription>
-              Change your password here. After saving, you'll be logged out.
+              // eslint-disable-next-line react/no-unescaped-entities Change
+              your password here. After saving, you&apos;ll be logged out.
             </CardDescription>
           </CardHeader>
           <CardContent className="space-y-2">
@@ -72,5 +71,5 @@ export function TabUnit() {
         </Card>
       </TabsContent>
     </Tabs>
-  )
+  );
 }
