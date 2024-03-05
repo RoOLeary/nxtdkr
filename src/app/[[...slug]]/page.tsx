26ceec2d7bc77f70a5ee3eec6888fc5b9d291245
Ronan <roleary81@gmail.com>
2024-03-05 13:56:08 +0100

    feat(nxtdkr-feat-00XX): redis

--- src/app/[[...slug]]/page.tsx
+++ src/app/[[...slug]]/page.tsx
@@ -187,10 +187,10 @@ export default function Page() {
         <CarouselPrevious />
         <CarouselNext />
       </Carousel>
-    <br />
-    <HomeAccordion />
-    <br />
-    <br />
+      <br />
+      <HomeAccordion />
+      <br />
+      <br />
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
         <a
           href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
