c03797823bb68b2cf1915709f9d298a7203fb928
Ronan O'Leary <roleary81@gmail.com>
2024-03-06 15:46:49 +0100

    test(nxtdkr-test-XXXX): testing somethingin the middleware

--- src/app/api/remote/route.ts
+++ src/app/api/remote/route.ts
@@ -1,39 +1,42 @@
 /* eslint-disable @typescript-eslint/dot-notation */
 /* eslint-disable no-console */
+import { cookies } from 'next/headers';
 import type { NextRequest } from 'next/server';
 import { NextResponse } from 'next/server';
 
 // This function can be marked `async` if using `await` inside
 export function GET(request: NextRequest) {
+  const cookieStore = cookies();
   const response = NextResponse.next();
 
-  response.headers.set('X-Custom-Header', 'Hello, Ballsack!');
+  response.headers.set(
+    'X-Custom-Header',
+    'Hello, Codestonerus Maximumus Ronanicus!',
+  );
   response.headers.set('Authorization', '2345123534534');
-  response.cookies.set('ballsack', '2');
+  cookieStore.set('doesoutsidesuck', '1');
 
   console.log(
     `Received ${request.method} request to ${request.url} at ${new Date()}`,
   );
 
-  const isAuthenticated = request.headers.get('authenticated') === 'true';
-  // const topsecretKey = response.headers.set('Authorization', '2345123534534');
-  const cookies = request.cookies.get('ballsack'); // Corrected to use the 'cookie' header
-  const tsKey = request.headers.get('Authorization') === '2345123534534';
+  const cks = cookieStore.get('doesoutsidesuck'); // Corrected to use the 'cookie' header
+  const tsKey = response.headers.get('Authorization');
 
-  // Check if a specific cookie exists
-  // @ts-ignore
-  console.log(`Authenticated: ${isAuthenticated}`);
   console.log(`Authorization: ${tsKey}`);
   // @ts-ignore
-  console.log(`Has Test Cookie: ${cookies?.value}`);
+  console.log(`Has Test Cookie: ${cks?.name}`);
 
   // Depending on whether the cookie is present, perform actions, e.g., redirect or allow the request
-  if (cookies) {
+
+  if (cks) {
+    console.log(`Has Doesoutsidesuck Cookie: ${cks?.name}`);
     // Perform action when the cookie is present
     // For example, allow the request to proceed
-    return NextResponse.redirect('http://localhost:3000/about');
+    // @ts-ignore
+    return NextResponse.redirect('http://localhost:3000/about', request?.url);
+    // return NextResponse.next();
   }
-  // Perform action when the cookie is not present
-  // For example, redirect to a different URL
+
   return NextResponse.redirect('http://localhost:3000/info');
 }
