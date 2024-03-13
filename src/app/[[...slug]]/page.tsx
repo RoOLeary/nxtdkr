669adcac7a2801e33a1f195770fbcafafb033790
Ronan O'Leary <roleary81@gmail.com>
2024-03-13 11:51:18 +0100

    remove image

--- src/app/[[...slug]]/page.tsx
+++ src/app/[[...slug]]/page.tsx
@@ -118,14 +118,7 @@ export default function Page() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert rounded-md mb-4"
-          src="https://ronan-oleary.com/assets/ro-bw.d434f415.png"
-          alt="Ro Logo"
-          width={180}
-          height={37}
-          priority
-        />
+        <p className="pb-4">Placeholder logo</p>
       </div>
       <div className="flex gap-4 z-[10]">
         <Button variant="secondary" className="hidden md:block">
@@ -158,7 +151,7 @@ export default function Page() {
               <FormItem>
                 <FormLabel>Nyom Nyom, yummy toasties</FormLabel>
                 <FormControl>
-                  <Input placeholder="Danger Ro" {...field} />
+                  <Input placeholder="Placeholder Text" {...field} />
                 </FormControl>
                 <FormDescription>
                   {field.value
