5c56f82f5232bea36f1eb8adf2f4badeb10fcd7f
Ronan O'Leary <roleary81@gmail.com>
2024-07-10 16:03:46 +0200

    deploy addition of metafield

--- src/app/info/[slug]/page.tsx
+++ src/app/info/[slug]/page.tsx
@@ -15,12 +15,13 @@ async function fetchData(slug: any) {
 export default async function Page({ params }: any) {
   const { slug } = params;
   const newsPage = await fetchData(slug);
-  const { title, content } = newsPage[0];
+  const { title, content, acf } = newsPage[0];
 
   return (
     <main className="article">
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
         <h1 className="text-5xl font-black pb-8">{title.rendered}</h1>
+        {acf?.sagetitle ? <p>{acf.sagetitle}</p> : ''}
       </div>
       <div
         className="content"
