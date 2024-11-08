7128fb8823b74976aa4e336154bc8e2ab8490a91
Ronan O'Leary <ronanoleary@Ronans-MacBook-Pro-2.local>
2024-11-08 14:10:52 +0100

    update page

--- src/app/info/page.tsx
+++ src/app/info/page.tsx
@@ -62,7 +62,7 @@ export default async function Page() {
                 // @ts-ignore
                 href={`/info/${slug}`}
                 className="hover:text-red-700 font-black"
-                prefetch={false}
+                prefetch
               >
                 <h5>{title.rendered}</h5>
                 {acf?.sagetitle ? (
